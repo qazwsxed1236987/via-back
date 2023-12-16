634b73b34efb1e81492e07b7e4643a57f441e797
qazwsxed1236987 <qazwsxed369@gmail.com>
2023-12-16 15:20:30 +0800

    test2

--- servers/connectDB.js
+++ servers/connectDB.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 
 // database message
 const pool = mysql.createPool({
-    host: '192.168.20.189',
+    host: 'tcp://0.tcp.jp.ngrok.io:16217',
     user: 'root',
     password: 'asd620134',
     database: 'via',
