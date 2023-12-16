8f19984726beb9ea8ab2da91584201abbaa777ff
qazwsxed1236987 <qazwsxed369@gmail.com>
2023-12-16 17:32:24 +0800

    test db 5

--- servers/connectDB.js
+++ servers/connectDB.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2/promise');
 
 // database message
 const pool = mysql.createPool({
-    host: '192.168.20.189',
-    user: 'qazwsxedtest',
-    password: 'test',
-    database: 'via',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     connectionLimit: 10,
     waitForConnections: true,
     dateStrings: true
