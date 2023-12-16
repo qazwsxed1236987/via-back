d73c073ed6a75c8f53528fba054bba12540ae57a
qazwsxed1236987 <qazwsxed369@gmail.com>
2023-12-16 16:24:49 +0800

    test db4

--- servers/connectDB.js
+++ servers/connectDB.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2/promise');
 // database message
 const pool = mysql.createPool({
     host: '192.168.20.189',
-    user: 'root',
-    password: 'asd620134',
+    user: 'qazwsxedtest',
+    password: 'test',
     database: 'via',
     connectionLimit: 10,
     waitForConnections: true,
