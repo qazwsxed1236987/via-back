c5a841e23c4de59a13116ff23d61a7eab7849199
qazwsxed1236987 <qazwsxed369@gmail.com>
2023-12-16 18:08:16 +0800

    dbtest

--- servers/todoserver.js
+++ servers/todoserver.js
@@ -22,10 +22,15 @@ router.route("/add").post(async (req, res) => {
     const addDate = date.toISOString().slice(0, 16).replace('T', ' ');
 
     const { names, title, text, toemail, sendtime } = req.body
-
+    let newsendtime
+    if (sendtime) {
+        newsendtime = sendtime
+    } else {
+        newsendtime = null
+    }
     const sql = `INSERT INTO memos 
     (names,title,text,settime,toemail,sendtime) VALUES
-    ('${names}','${title}','${text}','${addDate}','${toemail}','${sendtime}');`
+    ('${names}','${title}','${text}','${addDate}','${toemail}','${newsendtime}');`
     const datas = await db.query(sql)
 
     res.json(datas)
@@ -35,14 +40,19 @@ router.route("/reset").post(async (req, res) => {
     const date = new Date();
     date.setHours(date.getHours() + 8);
     const resetDate = date.toISOString().slice(0, 16).replace('T', ' ');
-
     const { id, title, text, toemail, sendtime } = req.body
+    let newsendtime
+    if (sendtime) {
+        newsendtime = sendtime
+    } else {
+        newsendtime = null
+    }
     const sql =
         `UPDATE memos SET 
         title = '${title}',
         text = '${text}',
         toemail = '${toemail}',
-        sendtime = '${sendtime}',
+        sendtime = '${newsendtime}',
         resettime ='${resetDate}'
         WHERE id = ${id};`
     const datas = await db.query(sql)
@@ -61,11 +71,17 @@ router.route("/deleted").post(async (req, res) => {
 //memo completed check
 router.route("/completed").post(async (req, res) => {
     const { id, toemail, sendtime, complete } = req.body
+    let newsendtime
+    if (sendtime) {
+        newsendtime = sendtime
+    } else {
+        newsendtime = null
+    }
     const sql =
         `UPDATE memos SET 
         complete = '${complete}',
         toemail = '${toemail}',
-        sendtime = '${sendtime}'
+        sendtime = '${newsendtime}'
         WHERE id = ${id};`
     const datas = await db.query(sql)
     res.json(datas)
